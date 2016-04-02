894990443cac7d8187bc47894fcfd5145ab29f7f
ivan0124 <ivan0124.tw@gmail.com>
2016-04-02 16:38:57 +0000

    add- add ioctl function in app

--- driver_export_function/app_src/app_a/app_a.c
+++ driver_export_function/app_src/app_a/app_a.c
@@ -1,18 +1,111 @@
-/*hello.c*/
-#include <stdio.h>
-#include "lib_common.h"
+/*ioctl.c*/
+/* 
+ * device specifics, such as ioctl numbers and the
+ * major device file. 
+ */
+#include "chardev.h"
 
-int main( int argc, char* argv[])
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>		/* open */
+#include <unistd.h>		/* exit */
+#include <sys/ioctl.h>		/* ioctl */
+#include <string.h>
+
+/* 
+ * Functions for the ioctl calls 
+ */
+
+ioctl_set_msg(int file_desc, char *message)
 {
-    log("Hello World\n"); 
-    testFuncA();
-    testFuncA1();
-    //calling function in library
-    test_lib_a();
-    test_lib_a1();
+	int ret_val;
+
+	ret_val = ioctl(file_desc, IOCTL_SET_MSG, message);
+
+	if (ret_val < 0) {
+		printf("ioctl_set_msg() failed:%d\n", ret_val);
+		exit(-1);
+	}
+}
+
+ioctl_get_msg(int file_desc)
+{
+	int ret_val;
+	char message[100]={0};
+
+	/* 
+	 * Warning - this is dangerous because we don't tell
+	 * the kernel how far it's allowed to write, so it
+	 * might overflow the buffer. In a real production
+	 * program, we would have used two ioctls - one to tell
+	 * the kernel the buffer length and another to give
+	 * it the buffer to fill
+	 */
+	ret_val = ioctl(file_desc, IOCTL_GET_MSG, message);
+
+	if (ret_val < 0) {
+		printf("ioctl_get_msg() failed:%d\n", ret_val);
+		exit(-1);
+	}
+
+	printf("ioctl_get_msg():%s\n", message);
+}
+
+ioctl_get_nth_byte(int file_desc)
+{
+	int ret_val=0;
+        char message[100]={0};
+
+        ret_val = ioctl(file_desc, IOCTL_GET_NTH_BYTE, message);
+	
+	if (ret_val < 0) {
+		printf("ioctl_get_nth_byte() failed:%d\n", ret_val);
+		exit(-1);
+	}
+	printf("ioctl_get_nth_byte():%s\n", message);
+}
+
+int get_shell_cmd_result(char* cmd, char* res, int res_size){
+
+    int len=0;
+    char buf[256]={0};
+    FILE* fp=NULL;
     
-    char res[256]={0};
-    get_shell_cmd_result("ls", res, sizeof(res));
-    log("%s",res);
+    fp=popen(cmd,"r");
+
+    if(fp != NULL){
+        while(fgets(buf,sizeof(buf),fp)){
+            strncat(res,buf,res_size-strnlen(res,res_size));
+        }
+        pclose(fp);
+    }
+    else{
+	return -1;
+    }
+    len=strnlen(res,res_size-1);
+    res[len-1]=0;
     return 0;
+}
+/* 
+ * Main - Call the ioctl functions 
+ */
+main()
+{
+	int file_desc, ret_val;
+	char *msg = "Message passed by ioctl\n";
+        char device_file[256]={0};  
+       
+        get_shell_cmd_result("cat ../../drv_src/drv_caller/chardev.h | grep '#define DEVICE_FILE' | cut -d '\"' -f 2", device_file, sizeof(device_file));
+        printf("device file = %s\n",device_file);
+	file_desc = open(device_file, 0);
+	if (file_desc < 0) {
+		printf("Can't open device file: %s\n", MODULE_NAME);
+		exit(-1);
+	}
+
+	ioctl_get_nth_byte(file_desc);
+	ioctl_get_msg(file_desc);
+	ioctl_set_msg(file_desc, msg);
+
+	close(file_desc);
 }
