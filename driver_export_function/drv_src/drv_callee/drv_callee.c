76f8ad698f52ab87f27d725ece0b915e141efd95
ivan0124 <ivan0124.tw@gmail.com>
2016-04-05 14:43:08 +0000

    remove- remvoe unused code

--- driver_export_function/drv_src/drv_callee/drv_callee.c
+++ driver_export_function/drv_src/drv_callee/drv_callee.c
@@ -7,58 +7,8 @@
 
 MODULE_LICENSE("Dual BSD/GPL");
 
-/*
-static int drv_callee_open(struct inode *inode, struct file *filp) {
-    printk("<1>drv_callee: open\n");
-    return 0;
-}
-
-static int drv_callee_close(struct inode *inode, struct file *filp) {
-    printk("<1>drv_callee: close\n");
-    return 0;
-}
-
-static int drv_callee_ioctl(struct inode *inode, struct file *filp, unsigned int ioctl_num, unsigned long ioctl_param) {
-    
-    printk("<1>drv_callee: ioctl\n");
-    
-    switch (ioctl_num) {
-	case IOCTL_SET_MSG:
-		printk("drv_callee: enter IOCTL_SET_MSG\n");
-		break;
-
-	case IOCTL_GET_MSG:
-		printk("drv_callee: enter IOCTL_GET_MSG\n");
-		break;
-
-	case IOCTL_GET_NTH_BYTE:
-		printk("drv_callee: enter IOCTL_GET_NTH_BYTE\n");
-		break;
-    }
-
-    return 0;
-}
-
-static ssize_t drv_callee_read(struct file *filp, char *buf, size_t size, loff_t *f_pos) {
-    printk("<1>drv_callee: read  (size=%zu)\n", size);
-    return 0;
-}
-
-static ssize_t drv_callee_write(struct file *filp, const char *buf, size_t size, loff_t *f_pos) {
-    printk("<1>drv_callee: write  (size=%zu)\n", size);
-    return size;
-}
-
-static struct file_operations caller_fops = {
-    .open = drv_callee_open,
-    .release = drv_callee_close,
-    //.ioctl = drv_callee_ioctl,
-    .read = drv_callee_read,
-    .write = drv_callee_write,
-};
-*/
 int export_hello(void) {
-    printk("Hello from another module");
+    printk("<1>drv_callee: export_hello() is called");
     return 0;
 } 
 
@@ -66,20 +16,11 @@ static int drv_callee_init(void) {
     
     int result=0;
     printk("<1>drv_callee: init\n");
-/*
-    result = register_chrdev(MAJOR_NUM, MODULE_NAME, &caller_fops);
-    if (result < 0) {
-        printk("<1>driver_callee: Failed to register character device\n");
-        return result;
-    }
-*/
     return 0;
 }
 
 static void drv_callee_exit(void) {
     printk("<1>drv_callee: exit\n");
-
-    //unregister_chrdev(MAJOR_NUM, MODULE_NAME);
 }
 
 /*Export symbol*/
