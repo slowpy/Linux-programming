43551c54db7915b5e8ad5cedec5a797236b1942a
ivan <ivan0124.tw@gmail.com>
2016-04-06 22:14:17 -0500

    Update mk.sh for user_backtrace

--- user_backtrace/mk.sh
+++ user_backtrace/mk.sh
@@ -25,10 +25,8 @@ case "$1" in
 	cd .. ;;
 	*)
 	echo "step1: 'mk.sh build' to build code. 'mk.sh clean to clean build result'"
-	echo "step2: 'mk.sh install' to install the driver. type 'demsg' to see logs"
-	echo "step3: 'mk.sh test' to test."
-	echo "step4: 'mk.sh uninstall' to uninstall the driver. type 'dmesg' to see logs"
-	echo "step5: 'mk.sh clean to clean build result'"
+	echo "step2: 'mk.sh test' to test."
+	echo "step3: 'mk.sh clean to clean build result'"
 	exit -1 ;;
 esac
 
