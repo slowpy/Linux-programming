377eb7b4d6bb2403045eb1c57d16e2d654e91e6c
ivan0124 <ivan0124.tw@gmail.com>
2016-04-11 03:51:40 +0000

    add- add driver_timer sample

---
+++ driver_timer/drv_src/drv_timer/drv_timer.c
@@ -0,0 +1,34 @@
+/* drv_timer.c */
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/timer.h>
+
+MODULE_LICENSE("Dual BSD/GPL");
+
+static struct timer_list my_timer;
+
+void my_timer_callback( unsigned long data )
+{
+     /* do your timer stuff here */
+     printk("<1>drv_timer: timer triggered\n");
+}
+
+
+static int drv_timer_init(void) {
+    printk("<1>drv_timer: init\n");
+    /* setup your timer to call my_timer_callback */
+    setup_timer(&my_timer, my_timer_callback, 0);
+    /* setup timer interval to 200 msecs */
+    mod_timer(&my_timer, jiffies + msecs_to_jiffies(200));
+    return 0;
+}
+
+static void drv_timer_exit(void) {
+    printk("<1>drv_timer: exit\n");
+    /* remove kernel timer when unloading module */
+    del_timer(&my_timer);
+}
+
+module_init(drv_timer_init);
+module_exit(drv_timer_exit);
+
