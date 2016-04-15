148697255dfbac69d96fb31adcf192f78a5a9081
ivan0124 <ivan0124.tw@gmail.com>
2016-04-15 15:23:06 +0000

    add- add user_fork_process sample code

---
+++ user_fork_process/app_src/app_a/app_a.c
@@ -0,0 +1,49 @@
+/*app_a.c*/
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "lib_common.h"
+
+#define ERR_EXIT(m) \
+    do\
+    {\
+        log(m);\
+        exit(EXIT_FAILURE);\
+    }\
+    while (0)\
+
+int main(void)
+{
+    pid_t pid;
+    log("\n");
+    log("before calling fork,calling process pid = %d\n",getpid());
+    pid = fork();
+
+    if(pid == -1)
+        ERR_EXIT("fork error");
+
+    if(pid == 0){
+        log("[child process] pid = %d,parent's pid = %d\n",getpid(),getppid());
+	return 99;
+    }
+    if(pid > 0){
+        log("[parent process] pid =%d ,child's pid = %d\n",getpid(),pid);
+        int stat_val;
+	pid_t child_pid;
+	
+        log("[parent process] wait child process finish...\n");
+        child_pid = wait(&stat_val);
+        log("[parent process] child has finished: pid=%d\n",child_pid);
+
+        if(WIFEXITED(stat_val)){
+	    log("[parent process] child exit with code %d\n", WEXITSTATUS(stat_val));
+        }
+        else{
+	    log("[parent process] child terminated abnormally\n");
+        }
+ 	
+    }
+
+    return 0;
+}
+
