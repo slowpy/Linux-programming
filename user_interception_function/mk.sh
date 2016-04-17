2920c079a1feb9e6577928fc1cf1c111e0f9f9e0
ivan0124 <ivan0124.tw@gmail.com>
2016-04-17 15:55:02 +0000

    remove- remove mk.sh unused code

--- user_interception_function/mk.sh
+++ user_interception_function/mk.sh
@@ -18,14 +18,6 @@ case "$1" in
 	else
    	     make clean
 	fi ;;
-	install)
-	cd ./build
-	./drv_hello.sh install
-	cd .. ;;
-	uninstall)
-	cd ./build
-	./drv_hello.sh uninstall
-	cd .. ;;
 	test)
 	cd ./build
 	export LD_LIBRARY_PATH=$(pwd)
@@ -33,10 +25,8 @@ case "$1" in
 	cd .. ;;
 	*)
 	echo "step1: 'mk.sh build' to build code. 'mk.sh clean to clean build result'"
-	echo "step2: 'mk.sh install' to install the driver. type 'demsg' to see logs"
-	echo "step3: 'mk.sh test' to test."
-	echo "step4: 'mk.sh uninstall' to uninstall the driver. type 'dmesg' to see logs"
-	echo "step5: 'mk.sh clean to clean build result'"
+	echo "step2: 'mk.sh test' to test."
+	echo "step3: 'mk.sh clean to clean build result'"
 	exit -1 ;;
 esac
 
